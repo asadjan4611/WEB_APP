78f36160d3e97927c6a6905bc784489503a6c562
Muhammad Asad <asadjan4611@gmail.com>
2025-09-21 15:27:54 +0500

    Update ProductCard.jsx

--- frontened/vite-project/src/component/Route/ProductCard/ProductCard.jsx
+++ frontened/vite-project/src/component/Route/ProductCard/ProductCard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "../../../style/style";
-import ProductCardDetails from "../productCardDetails/ProductCardDetails";
+import ProductCardDetails from "../productCardDetails/ProductCardDetails.jsx";
 import ReactDOM from "react-dom";
 
 import {
