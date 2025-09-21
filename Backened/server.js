c6d01b3223f8e2db69cbdc49943150888d57c679
asadjan4611 <asadjan4611@gmail.com>
2025-09-22 02:10:13 +0500

    Test Stripe  server

--- Backened/server.js
+++ Backened/server.js
@@ -1,38 +1,25 @@
-require("dotenv").config({ path: "./Backened/config/.env" });
+require("dotenv").config({ path: __dirname + "/config/.env" });
 const app = require("./app");
 const connectDatabase = require("./db/Database");
 
 process.on("uncaughtException", (err) => {
   console.log(`Error is ${err}`);
-  console.log("SHutting down the server for handling uncaught exception");
+  console.log("Shutting down the server for handling uncaught exception");
 });
 
-//config
-
-if (process.env.NODE_ENV !== "PROD") {
-  require("dotenv").config({
-    path: "/backened/config/.env",
-  });
-}
-
-//connect DB
+// connect DB
 connectDatabase();
-//create server
 
+// create server
 const server = app.listen(process.env.PORT, () => {
-  try {
-    console.log(`Server is running on http://localhost:${process.env.PORT}`);
-  } catch (error) {
-    console.log("your error is ", error);
-  }
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
 
-//unhandle promise rejextion
-
+// unhandled promise rejection
 process.on("unhandledRejection", (err) => {
   console.log("Shutting down the server for ", err.message);
   console.log(
-    "Shutting down the server unhandle promise rejection ",
+    "Shutting down the server unhandled promise rejection ",
     err.message
   );
   server.close(() => {
